90a736bea42b0e91c1ed44d28632a4d389f3abcc
Pirororo <sw.cont.211@gmail.com>
2020-07-30 04:41:14 +0900

    line_Test

--- CA_200728/js/src/camera/camera.js
+++ CA_200728/js/src/camera/camera.js
@@ -11,20 +11,21 @@ export class Camera extends THREE.PerspectiveCamera{
 
     this.frame =0;
 
-    this.randomInt = this.randomInt.bind(this);
+    // this.randomInt = this.randomInt.bind(this);
 
-    this.nowCamPos = new THREE.Vector3(20, 90, 60);
+    // this.nowCamPos = new THREE.Vector3(20, 90, 60);
+    this.nowCamPos = new THREE.Vector3(0, 0, 100);
     this.position.set(this.nowCamPos.x, this.nowCamPos.y, this.nowCamPos.z);
     this.nowCamLook = new THREE.Vector3(0, 0, 0);
     this.lookAt(this.nowCamLook.x, this.nowCamLook.y, this.nowCamLook.z);
-    this.targetCamPos = new THREE.Vector3(
-      this.randomInt(-100, 300),
-      this.randomInt(-100, 300),
-      this.randomInt(-100, 300)
-    );
-    this.targetCamLook = new THREE.Vector3(
-      0,0,0
-    );
+    // this.targetCamPos = new THREE.Vector3(
+    //   this.randomInt(-100, 300),
+    //   this.randomInt(-100, 300),
+    //   this.randomInt(-100, 300)
+    // );
+    // this.targetCamLook = new THREE.Vector3(
+    //   0,0,0
+    // );
 
 
 
@@ -50,39 +51,39 @@ export class Camera extends THREE.PerspectiveCamera{
     // TWEEN.update();
     // this.lookAt(new THREE.Vector3(0,0,0));
 
-    this.frame += 1;
+  //   this.frame += 1;
 
-    if(this.frame% 300 == 0){
-      this.targetCamPos = new THREE.Vector3(
-        this.randomInt(-100, 200),
-        this.randomInt(-100, 200),
-        this.randomInt(-100, 200)
-      );
+  //   if(this.frame% 300 == 0){
+  //     this.targetCamPos = new THREE.Vector3(
+  //       this.randomInt(-100, 200),
+  //       this.randomInt(-100, 200),
+  //       this.randomInt(-100, 200)
+  //     );
 
-      this.targetCamLook = new THREE.Vector3(
-        this.randomInt(0, 70),
-        this.randomInt(0, 70),
-        this.randomInt(0, 70)
-      );
+  //     this.targetCamLook = new THREE.Vector3(
+  //       this.randomInt(0, 70),
+  //       this.randomInt(0, 70),
+  //       this.randomInt(0, 70)
+  //     );
 
-    }
+  //   }
 
-    // this.camPos += (this.targetCamPos- this.nowCamPos)*0.02;//この書き方動かない！！！！！！！
-    this.nowCamPos.x += (this.targetCamPos.x - this.nowCamPos.x) *0.03;
-    this.nowCamPos.y += (this.targetCamPos.y - this.nowCamPos.y) *0.03;
-    this.nowCamPos.z += (this.targetCamPos.z - this.nowCamPos.z) *0.03;
-    // this.position.set(this.nowCamPos);//この書き方動かない！！！！！！！
-    this.position.set(this.nowCamPos.x,this.nowCamPos.y,this.nowCamPos.z);
+  //   // this.camPos += (this.targetCamPos- this.nowCamPos)*0.02;//この書き方動かない！！！！！！！
+  //   this.nowCamPos.x += (this.targetCamPos.x - this.nowCamPos.x) *0.03;
+  //   this.nowCamPos.y += (this.targetCamPos.y - this.nowCamPos.y) *0.03;
+  //   this.nowCamPos.z += (this.targetCamPos.z - this.nowCamPos.z) *0.03;
+  //   // this.position.set(this.nowCamPos);//この書き方動かない！！！！！！！
+  //   this.position.set(this.nowCamPos.x,this.nowCamPos.y,this.nowCamPos.z);
 
-    this.nowCamLook.x += (this.targetCamLook.x - this.nowCamLook.x) *0.03;
-    this.nowCamLook.y += (this.targetCamLook.y - this.nowCamLook.y) *0.03;
-    this.nowCamLook.z += (this.targetCamLook.z - this.nowCamLook.z) *0.03;
+  //   this.nowCamLook.x += (this.targetCamLook.x - this.nowCamLook.x) *0.03;
+  //   this.nowCamLook.y += (this.targetCamLook.y - this.nowCamLook.y) *0.03;
+  //   this.nowCamLook.z += (this.targetCamLook.z - this.nowCamLook.z) *0.03;
 
-    this.lookAt(new THREE.Vector3(this.nowCamLook.x, this.nowCamLook.y, this.nowCamLook.z));
+  //   this.lookAt(new THREE.Vector3(this.nowCamLook.x, this.nowCamLook.y, this.nowCamLook.z));
   }
 
 
-  randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  // randomInt(min, max) {
+  //   return Math.floor(Math.random() * (max - min + 1)) + min;
+  // }
 }
--- CA_200728/js/src/scene/scene.js
+++ CA_200728/js/src/scene/scene.js
@@ -1,5 +1,6 @@
 // import * as THREE from '../../libs/three.module.js';
 import {Camera} from '../camera/camera.js';
+import Line from '../objects/line2.js';
 
 /**
  * シーンクラス：カメラとbox
@@ -13,45 +14,55 @@ export class Scene extends THREE.Scene {
         this.frame = 0;
         this.scene = 0;
 
-        //カメラ
-        this.camera = new Camera();//thisにすること！！！最終的にはgame2.jsでsceneにaddする
+        //ライト
+        // 環境光源
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
+        this.add(ambientLight);
 
-        //BOX
-        this.NUM = 4;
-        this.boxList = [];
-        this.nowBoxPos = [];
-        this.targetBoxPos = [];
-        this.boxGroup = new THREE.Group();
+        // //カメラ
+        // this.camera = new Camera();//thisにすること！！！最終的にはgame2.jsでsceneにaddする
 
-        for (let i = 0; i < this.NUM; i++) {
+
+        //ライン
+        this.line = new Line();
+        this.add(this.line);
         
-            this.floorsize = 60;
-            this.geometry = new THREE.PlaneBufferGeometry(this.floorsize, this.floorsize,3,3);
+        // //BOX
+        // this.NUM = 4;
+        // this.boxList = [];
+        // this.nowBoxPos = [];
+        // this.targetBoxPos = [];
+        // this.boxGroup = new THREE.Group();
 
-            this.hue = Math.random()*200/360;
-            this.boxColor = new THREE.Color();
-            this.boxColor.setHSL(this.hue, 0.8, 0.6);
-            const material = new THREE.MeshBasicMaterial({
-                color: this.boxColor,
-                wireframe: true
-            });
+        // for (let i = 0; i < this.NUM; i++) {
+        
+        //     this.floorsize = 60;
+        //     this.geometry = new THREE.PlaneBufferGeometry(this.floorsize, this.floorsize,3,3);
 
-            this.box = new THREE.Mesh(this.geometry, material);
-            this.box.position.set(33*i, 0, 0);
-            this.box.rotation.x = 90 * Math.PI / 180;
-            this.boxGroup.add(this.box);
+        //     this.hue = Math.random()*200/360;
+        //     this.boxColor = new THREE.Color();
+        //     this.boxColor.setHSL(this.hue, 0.8, 0.6);
+        //     const material = new THREE.MeshBasicMaterial({
+        //         color: this.boxColor,
+        //         wireframe: true
+        //     });
 
-            // 個々のmeshをリスト化して保存
-            this.boxList.push(this.box);
+        //     this.box = new THREE.Mesh(this.geometry, material);
+        //     this.box.position.set(33*i, 0, 0);
+        //     this.box.rotation.x = 90 * Math.PI / 180;
+        //     this.boxGroup.add(this.box);
 
-            // 現在のpositions
-            this.nowBoxPos.push(this.box.position.x, this.box.position.y, this.box.position.z);
+        //     // 個々のmeshをリスト化して保存
+        //     this.boxList.push(this.box);
 
-            // ターゲット位置のpositions
-            this.targetBoxPos.push(0, 0, 0);
-        }
+        //     // 現在のpositions
+        //     this.nowBoxPos.push(this.box.position.x, this.box.position.y, this.box.position.z);
 
-        this.add(this.boxGroup);
+        //     // ターゲット位置のpositions
+        //     this.targetBoxPos.push(0, 0, 0);
+        // }
+
+        // this.add(this.boxGroup);
 
     }
 
@@ -61,63 +72,66 @@ export class Scene extends THREE.Scene {
         //カメラ
         this.camera.update();
 
-        //box
-        for(let i =0; i< this.NUM*3; i++){
-            this.nowBoxPos[i] += (this.targetBoxPos[i]-this.nowBoxPos[i]) *0.04;
-        }
+        //ライン
+        this.line.update();
 
-        this.frame += 1;
+        // //box
+        // for(let i =0; i< this.NUM*3; i++){
+        //     this.nowBoxPos[i] += (this.targetBoxPos[i]-this.nowBoxPos[i]) *0.04;
+        // }
 
-        if(this.frame%120 == 0){
+        // this.frame += 1;
 
-            this.scene += 1;
-            if(this.scene >= 4){
-                this.scene = 1;
-            }
+        // if(this.frame%120 == 0){
 
-            if(this.scene == 1){
-                for(let i =0; i< this.NUM; i++){
-                    // this.targetBoxPos[i].set((i-3)*33, (i-3)*33, (i-3)*33);
-                    this.targetBoxPos[3 * i + 0] = (i-3)*33;
-                    this.targetBoxPos[3 * i + 1] = (i-3)*33;
-                    this.targetBoxPos[3 * i + 2] = (i-3)*33;
-                }
-            }
-            if(this.scene == 2){
-                for(let i =0; i< this.NUM; i++){
-                    // this.targetBoxPos[i].set(0, i*33, 0);
-                    this.targetBoxPos[3 * i + 0] = 0;
-                    this.targetBoxPos[3 * i + 1] = i*33;
-                    this.targetBoxPos[3 * i + 2] = 0;
-                }
-            }
-            if(this.scene == 3){
-                for(let i =0; i< this.NUM; i++){
-                    if(i%2 ==0){
-                        // this.targetBoxPos[i].set(i*33, 0, 0);
-                        this.targetBoxPos[3 * i + 0] = i*33;
-                        this.targetBoxPos[3 * i + 1] = 0;
-                        this.targetBoxPos[3 * i + 2] = 0;
-                    }else{
-                        // this.targetBoxPos[i].set((i-1)*33, 0, 2*33);
-                        this.targetBoxPos[3 * i + 0] = (i-1)*33;
-                        this.targetBoxPos[3 * i + 1] = 0;
-                        this.targetBoxPos[3 * i + 2] = 2*33;
-                    }
-                }
-            }
-        }
+        //     this.scene += 1;
+        //     if(this.scene >= 4){
+        //         this.scene = 1;
+        //     }
+
+        //     if(this.scene == 1){
+        //         for(let i =0; i< this.NUM; i++){
+        //             // this.targetBoxPos[i].set((i-3)*33, (i-3)*33, (i-3)*33);
+        //             this.targetBoxPos[3 * i + 0] = (i-3)*33;
+        //             this.targetBoxPos[3 * i + 1] = (i-3)*33;
+        //             this.targetBoxPos[3 * i + 2] = (i-3)*33;
+        //         }
+        //     }
+        //     if(this.scene == 2){
+        //         for(let i =0; i< this.NUM; i++){
+        //             // this.targetBoxPos[i].set(0, i*33, 0);
+        //             this.targetBoxPos[3 * i + 0] = 0;
+        //             this.targetBoxPos[3 * i + 1] = i*33;
+        //             this.targetBoxPos[3 * i + 2] = 0;
+        //         }
+        //     }
+        //     if(this.scene == 3){
+        //         for(let i =0; i< this.NUM; i++){
+        //             if(i%2 ==0){
+        //                 // this.targetBoxPos[i].set(i*33, 0, 0);
+        //                 this.targetBoxPos[3 * i + 0] = i*33;
+        //                 this.targetBoxPos[3 * i + 1] = 0;
+        //                 this.targetBoxPos[3 * i + 2] = 0;
+        //             }else{
+        //                 // this.targetBoxPos[i].set((i-1)*33, 0, 2*33);
+        //                 this.targetBoxPos[3 * i + 0] = (i-1)*33;
+        //                 this.targetBoxPos[3 * i + 1] = 0;
+        //                 this.targetBoxPos[3 * i + 2] = 2*33;
+        //             }
+        //         }
+        //     }
+        // }
 
     }
 
     draw(){
 
-        //box
-        for(let i =0; i< this.NUM; i++){
-            this.boxList[i].position.x = this.nowBoxPos[3 * i + 0];
-            this.boxList[i].position.y = this.nowBoxPos[3 * i + 1];
-            this.boxList[i].position.z = this.nowBoxPos[3 * i + 2];
-        }
+        // //box
+        // for(let i =0; i< this.NUM; i++){
+        //     this.boxList[i].position.x = this.nowBoxPos[3 * i + 0];
+        //     this.boxList[i].position.y = this.nowBoxPos[3 * i + 1];
+        //     this.boxList[i].position.z = this.nowBoxPos[3 * i + 2];
+        // }
 
     }
 
