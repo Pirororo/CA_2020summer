4664ac0c4b69b8654ca5e003c5188813fcc24029
Pirororo <sw.cont.211@gmail.com>
2020-08-12 23:17:52 +0900

    submit 0812 design!

--- CA_200808/RoomTest2/js/src/objects/rail_List2.js
+++ CA_200808/RoomTest2/js/src/objects/rail_List2.js
@@ -22,17 +22,17 @@ export default class Line extends THREE.Object3D {
 
         let Params = function(){
             this.amount = 15;
-            this.color = 0xC7FF73;//黄緑
+            this.color = 0xffffff;//黄緑C7FF73
             // this.color = "#ffffff";
             this.lineWidthMax = 0.3;
             this.lineWidthMin = 0.1;
             this.dashArray = 0.2;//黄緑0.2  白0.3
             this.dashRatio = 0.1;//黄緑0.1  白0.6
             this.dashGradate = 0.9;////黄緑0.9  白1.7 白は1.7以上じゃないと見えない。他の色は下げても見える。
-            this.backGradate = 0.4;
+            this.backGradate = 0.8;
             this.dashOffsetSpeed = 0.004;
 
-            this.xAmp = 62;
+            this.xAmp = 30;
             this.xFreq = 2.5;
             this.yAmp = -40;
             this.yFreq = 3.5;
