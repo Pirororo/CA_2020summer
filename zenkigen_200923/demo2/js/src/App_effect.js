789bb81855d19457892611cedabc5a244645df08
Pirororo <sw.cont.211@gmail.com>
2020-10-12 19:44:37 +0900

    demo2

--- zenkigen_200923/demo2/js/src/App_effect.js
+++ zenkigen_200923/demo2/js/src/App_effect.js
@@ -1,24 +1,29 @@
+
 // import * as THREE from '../libs/three.module.js';
 
 /**
  * メインアプリクラスです。
  */
 export class App{
+  //  export default class App{
       /**
      * @constructor
      * @param sceneInstance
      */
     constructor(sceneInstance){
+
+      
+      //この中からconstructer外部のmethodを呼び出すためにはbindする必要がある
       this._update = this._update.bind(this);
       this._resize = this._resize.bind(this);
       // this._keyEvent = this._keyEvent.bind(this);
-
+  
       // シーン
       this._scene = sceneInstance;
   
       //レンダラー
       this._renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-      this._renderer.setClearColor(new THREE.Color(0xffffff), 1.0);//C7FFE4//C7FF73//A4F917
+      this._renderer.setClearColor(new THREE.Color(0x000000), 1.0);
       this._renderer.setSize(window.innerWidth, window.innerHeight);
       this._renderer.setPixelRatio(1);
       this._renderer.shadowMap.enabled = true;//影に必要
@@ -26,25 +31,49 @@ export class App{
       // DOMを追加
       this._wrapper = document.getElementById('WebGL-output').appendChild(this._renderer.domElement);
 
-      //オービットカメラ
-      this.control = new THREE.OrbitControls(this._scene.camera);
+      // this.control = new THREE.OrbitControls(this._scene.camera);
   
       // リサイズ
       this._resize();
       window.addEventListener('resize', this._resize);
+
       window.addEventListener('keyup', this._keyEvent);
 
-      //マウス
-      window.addEventListener('mousemove', e => {
-        this._scene.scene2._pictWaver.mouseMoved(e.clientX, e.clientY);
-      });
+    // シェーダー
+    //レンダーパス
+    var renderPass = new THREE.RenderPass(this._scene, this._scene.camera);
+    renderPass.clear = true;//Lineは線が更新されていくのでtrueにする、falseだと線最初から全部のこっちゃう
 
-      
-      // フレーム毎の更新
-      this._update();
+    //ブルームパス
+    // var bloomPass = new BloomPass(1.5, 15, 3.0, 64);
+    var bloomPass = new THREE.BloomPass(2.0, 15, 3.0, 64);
+    bloomPass.enabled = true;
 
-      this.frame = 0;
-      this.sceneNUM = 2;
+    //出力パス
+    //コピーパス
+    var effectCopy = new THREE.ShaderPass(THREE.CopyShader);//コピー
+    effectCopy.renderToScreen = true;
+
+
+    //コンポーザーの定義
+    this.composer = new THREE.EffectComposer(this._renderer);
+    this.composer.renderTarget1.stencilBuffer = true;//?
+    this.composer.renderTarget2.stencilBuffer = true;//?
+
+    //コンポーザーに入れていく
+    this.composer.addPass(renderPass);
+    this.composer.addPass(bloomPass);
+    this.composer.addPass(effectCopy);
+
+
+  
+    // フレーム毎の更新
+    this._update();
+
+    this.frame = 0;
+    this.sceneNUM = 0;
+
+
   
     }
 
@@ -53,110 +82,73 @@ export class App{
     //   //   return;
     //   // }
     //   // // 何かをする
-      
+
+    //   // let KEY = event.key;
     //   if (event.key === 'a') {
-    //     this._scene.scene2.scene = 1;//_keyEventをbindしないと_sceneの中身を参照できない！
-    //     this._scene.camera = this._scene._persCamera
+    //     this._scene.scene2.scene = 1;
     //   }
     //   if (event.key === 's') {
     //     this._scene.scene2.scene = 2;
-    //     this._scene.camera = this._scene._persCamera
     //   }
     //   if (event.key === 'd') {
     //     this._scene.scene2.scene = 3;
-    //     this._scene.camera = this._scene._persCamera
     //   }
     //   if (event.key === 'f') {
     //     this._scene.scene2.scene = 4;
-    //     this._scene.camera = this._scene._persCamera
     //   }
     //   if (event.key === 'g') {
     //     this._scene.scene2.scene = 5;
-    //     this._scene.camera = this._scene._persCamera
     //   }
     //   if (event.key === 'h') {
     //     this._scene.scene2.scene = 6;
-    //     this._scene.camera = this._scene._persCamera
-    //   }
-    //   if (event.key === 'j') {
-    //     this._scene.scene2.scene = 7;
-    //     this._scene.camera = this._scene._orthoCamera;
-    //   }
-    //   if (event.key === 'k') {
-    //     this._scene.scene2.scene = 8;
-    //     this._scene.camera = this._scene._persCamera
-    //   }
-    //   if (event.key === 'l') {
-    //     this._scene.scene2.scene = 9;
-    //     this._scene.camera = this._scene._persCamera
-    //   }
-    //   if (event.key === 'z') {
-    //     this._scene.scene2.scene = 10;
-    //     this._scene.camera = this._scene._persCamera
-    //   }
-    //   if (event.key === 'x') {
-    //     this._scene.scene2.scene = 11;
-    //     this._scene.camera = this._scene._caCamera;
-    //   }
-    //   if (event.key === 'c') {
-    //     this._scene.scene2.scene = 12;
-    //     this._scene.camera = this._scene._orthoCamera;
     //   }
 
     // }
 
-
+  
+  
     /**
     * フレーム毎の更新をします。
     */
     _update() {
   
       this._renderer.autoClear = true;//これ大事〜！trueだと色が毎回背景白にクリアされちゃう、逆にtrueにしないと背景色ぬられない
-      // this._renderer.autoClear = false;
-  
       // シーンの更新
       this._scene.update();
       // this._scene.draw();
   
       requestAnimationFrame(this._update);
-      this._renderer.render(this._scene, this._scene.camera);
+      // this._renderer.render(this._scene, this._scene.camera);
+      this.composer.render();
 
-      // //カメラポジションの取得
-      // var position = this._scene.camera.matrixWorld.getPosition().clone();
-      // console.log(position);
+
+
 
       this.frame += 1;
       if(this.frame%480 ==0){
         this.sceneNUM += 1;
       }
-      if(this.sceneNUM >=3){
+      if(this.sceneNUM >=4){
         this.sceneNUM = 0;
         this.frame = 0;
       }
 
-      // if (this.sceneNUM == 0) {
-      //   this._scene.scene2.scene = 7;
-      //   this._scene.camera = this._scene._orthoCamera;
-      // }
       if (this.sceneNUM == 0) {
-        this._scene.scene2.scene = 8;
-        this._scene.camera = this._scene._persCamera;
+        this._scene.scene2.scene = 7;
+        this._scene.camera = this._scene._orthoCamera;
       }
       if (this.sceneNUM == 1) {
-        this._scene.scene2.scene = 9;
-        this._scene.camera = this._scene._persCamera;
+        this._scene.scene2.scene = 8;
+        this._scene.camera = this._scene._persCamera
       }
-      // if (this.sceneNUM == 3) {
-      //   this._scene.scene2.scene = 10;
-      //   this._scene.camera = this._scene._persCamera;
-      // }
       if (this.sceneNUM == 2) {
-        this._scene.scene2.scene = 13;
-        this._scene.camera = this._scene._persCamera;
+        this._scene.scene2.scene = 9;
+        this._scene.camera = this._scene._persCamera
       }
-
-      
-  
+      if (this.sceneNUM == 3) {
+        this._scene.scene2.scene = 10;
+        this._scene.camera = this._scene._persCamera
+      }
   
     }
   
@@ -170,8 +162,8 @@ export class App{
       this._renderer.domElement.setAttribute('height', String(height));
       this._renderer.setPixelRatio(window.devicePixelRatio || 1.0);
       this._renderer.setSize(width, height);
-      // this._scene.camera.aspect = width / height;
-      // this._scene.camera.updateProjectionMatrix();
+      this._scene.camera.aspect = width / height;
+      this._scene.camera.updateProjectionMatrix();
 
       console.log("resize");
     }
